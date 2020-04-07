43b1d82e40ae2178b01d6e17eb0f705a72837e82
PAULOFO <paulofo.forever@gmail.com>
2020-04-07 19:19:33 -0300

    Componente Item Refeição

---
+++ lib/components/meal_item.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import '../models/meal.dart';
+
+class MealItem extends StatelessWidget {
+
+  final Meal meal;
+
+  const MealItem(this.meal);
+
+  void _selectMeal() {} ///Função p/ mostrar os detalhes da receita em outra tela
+
+  ///Gera o construtor para o Card receber os dados do texto da receita
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: _selectMeal,
+      splashColor: Theme.of(context).primaryColor,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 4,
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(meal.imageUrl,
+                    height: 250,
+                    width: double.infinity,///Ocupa o espçao disponível
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  right: 10,
+                  bottom: 20,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,///black com 54% de opacidade
+                    padding: EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 20,
+                    ),
+                    child: Text(
+                      meal.title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,///Faz quebra de linha se necessário
+                      overflow: TextOverflow.fade,//Se houver um elemento muito grande faz um efeito de fade
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.schedule),
+                      SizedBox(width: 6),
+                      Text('${meal.duration} min'),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.work),
+                      SizedBox(width: 6),
+                      Text(meal.complexityText),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.attach_money),
+                      SizedBox(width: 6),
+                      Text(meal.costText),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
